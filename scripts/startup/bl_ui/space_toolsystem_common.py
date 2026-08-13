c1bdb435ea4528a356b3677102aefc21a37d3daa
Nhat_dep <96358505+Nhat-Dep@users.noreply.github.com>
2026-08-13 23:38:46 +0700

    Fix AttributeError _tool_group_active in space_toolsystem_common.py

--- scripts/startup/bl_ui/space_toolsystem_common.py
+++ scripts/startup/bl_ui/space_toolsystem_common.py
@@ -429,8 +429,15 @@ class ToolSelectPanelHelper:
         return None, -1
 
     @classmethod
+    def _get_tool_group_active_dict(cls):
+        tool_group_active = getattr(cls, "_tool_group_active", None)
+        if tool_group_active is None:
+            tool_group_active = cls._tool_group_active = {}
+        return tool_group_active
+
+    @classmethod
     def _tool_group_active_get_from_item(cls, item):
-        index = cls._tool_group_active.get(item[0].idname, 0)
+        index = cls._get_tool_group_active_dict().get(item[0].idname, 0)
         # Can happen in the case a group is dynamic.
         #
         # NOTE(Campbell): that in this case it's possible the order could change too,
@@ -447,7 +454,7 @@ class ToolSelectPanelHelper:
         if item_group:
             for i, item in enumerate(item_group):
                 if item and item.idname == idname:
-                    cls._tool_group_active[item_group[0].idname] = i
+                    cls._get_tool_group_active_dict()[item_group[0].idname] = i
                     return True
         return False
 
@@ -715,7 +722,7 @@ class ToolSelectPanelHelper:
 
                 if is_active:
                     # not ideal, write this every time :S
-                    cls._tool_group_active[item[0].idname] = index
+                    cls._get_tool_group_active_dict()[item[0].idname] = index
                 else:
                     index = cls._tool_group_active_get_from_item(item)
 
@@ -1015,7 +1022,7 @@ def _activate_by_item(context, space_type, item, index, *, as_fallback=False):
         if index_new == -1:
             raise Exception("Fallback tool not found in group")
 
-        cls._tool_group_active[tool_fallback_id] = index_new
+        cls._get_tool_group_active_dict()[tool_fallback_id] = index_new
 
         # Done, now get the current tool to replace the item & index.
         tool_active = ToolSelectPanelHelper._tool_active_from_context(context, space_type)
@@ -1131,7 +1138,7 @@ def activate_by_id_or_cycle(context, space_type, idname, *, offset=1, as_fallbac
 
     index_found = (tool_active.index + offset) % len(item_group)
 
-    cls._tool_group_active[item_group[0].idname] = index_found
+    cls._get_tool_group_active_dict()[item_group[0].idname] = index_found
 
     item_found = item_group[index_found]
     _activate_by_item(context, space_type, item_found, index_found)
